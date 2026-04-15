4248397cf6c974369b3d426bee71d37305969e71
erikaoliveiracorrea <erikaestudosaraujo@gmail.com>
2026-04-15 09:21:56 -0300

    Criando modelo

---
+++ todolist-back/src/main/java/com/example/todolist_back/model/Tarefa.java
@@ -0,0 +1,36 @@
+package com.example.todolist_back.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @Generated(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String texto;
+    private Boolean concluida;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+}
