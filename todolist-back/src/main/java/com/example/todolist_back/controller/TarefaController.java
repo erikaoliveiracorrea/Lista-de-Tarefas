f6a43da43555115170d11fed1c798eb4a27a28c6
erikaoliveiracorrea <erikaestudosaraujo@gmail.com>
2026-04-21 18:24:46 -0300

    Criando controller

---
+++ todolist-back/src/main/java/com/example/todolist_back/controller/TarefaController.java
@@ -0,0 +1,47 @@
+package com.example.todolist_back.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.todolist_back.model.Tarefa;
+import com.example.todolist_back.service.TarefaService;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/tarefas")
+
+public class TarefaController {
+
+    private final TarefaService tarefaService;
+
+    public TarefaController(TarefaService tarefaService) {
+        this.tarefaService = tarefaService;
+    }
+
+    @GetMapping
+    public List<Tarefa> listar() {
+        return tarefaService.listarTarefas();
+    }
+
+    @PostMapping
+    public Tarefa salvar(@RequestBody Tarefa tarefa) {
+        return tarefaService.salvarTarefa(tarefa);
+    }
+
+    @PutMapping("/{id}")
+    public Tarefa atualizar(@PathVariable Long id, @RequestBody Tarefa tarefa) {
+        return tarefaService.atualizarTarefa(id, tarefa);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        tarefaService.deletarTarefa(id);
+    }
+}
